9d237c6686e931b6a042c09e0a3b1ade8398566e
Juanjohn11 <elmatanaco1144@gmail.com>
2022-03-22 02:31:06 -0700

    added my last code

--- port/src/App.js
+++ port/src/App.js
@@ -1,17 +1,18 @@
 import React from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
-// import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
-// import Resume from "./components/Resume";
+import Resume from "./components/Resume";
 
 export default function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
-      {/* <Navbar /> */}
+      <Navbar />
       <About />
       <Projects />
       <Contact />
+      <Resume />
     </main>
   );
 }
